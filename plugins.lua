b5221ac73da5e523c596a0ac58b979aefae28f68
me0mar <ramogm@proton.me>
2024-01-20 18:47:20 +0100

    null-ls.nvim has been archived: Switching to new community-forked

--- plugins.lua
+++ plugins.lua
@@ -36,7 +36,7 @@ local plugins = {
     end
   },
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     event = "VeryLazy",
     opts = function()
       return require "custom.configs.null-ls"
